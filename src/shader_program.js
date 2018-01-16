eaf65bf7a6309610534618de56a478232f13fd7d
silasnordgren <nordgrensilas@gmail.com>
2018-01-16 01:54:47 +0100

    Deprecate createShaderProgram

--- src/shader_program.js
+++ src/shader_program.js
@@ -1,3 +1,11 @@
+/**
+ * Create a new shader program.
+ * 
+ * @param {WebGLRenderingContext} gl The WebGL context to use for creating the shader program.
+ * @param {WebGLShader} vertexShader The compiled vertex shader.
+ * @param {WebGLShader} fragmentShader The compiled fragment shader.
+ * @deprecated use twgl.js instead
+ */
 function createShaderProgram(gl, vertexShader, fragmentShader) {
   const program = gl.createProgram();
   gl.attachShader(program, fragmentShader);
